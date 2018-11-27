497db8294648266098b479c74a8b8dae6ca58dfb
jollysean <prabhatkr@gmail.com>
2018-11-26 22:41:26 -0500

    Lambda version

--- index.js
+++ index.js
@@ -1,109 +1,101 @@
-#!/usr/bin/env node
+const solc = require('solc');
+const Web3 = require('web3');
 
-const program = require('commander');
-const chalk = require('chalk');
-const figlet = require('figlet');
-
-const {prompt} = require('inquirer');
-
-const {solcVersionList } = require('./src/solcVersion');
-const {verifier} = require('./src/verifier');
-const {compiler_look_up} = require('./src/compilerList');
+const net_to_provider = {
+        '1':'https://chain.hyperdapp.org',
+        '2': 'https://ashoka.hyperdapp.org',
+        '3': 'https://shivaji.hyperdapp.org',
+        '4': 'https://chola.hyperdapp.org',
+      }
 
 
-// craft question for verifier parameters.
-const verifier_question = [
-  {
-    type: 'input',
-    name:'solc_version',
-    message: 'Enter compiler version. (e.g. v0.4.10+commit.f0d539ae).'
-  },
-  {
-    type: 'input',
-    name: 'file_name',
-    message: 'Enter the contract file name. (e.g. MultiSigWalletWithDailyLimit.sol)'
-  },
-  {
-    type: 'input',
-    name: 'contract_address',
-    message: 'Enter the contract address on blockchain. (e.g. 0x851b7f3ab81bd8df354f0d7640efcd7288553419)'
-  },
-  {
-    type: 'input',
-    name:'is_optimized',
-    message: '0 for not optimized, 1 for optimized.'
-  }
-];
-const compiler_question = [
-  {
-    type: 'input',
-    name: 'compiler_version_tag',
-    message: 'Enter the major released version. (e.g. 0.4.10, 0.3.8 etc.)'
-  }
-]
-program
-  .version('0.2.1')
-  .description('=========  Theos Bytecode Verifier  ==========='+'\n'+
-  chalk.yellow(
-		figlet.textSync('theos-Verifier',{horizontalLayout:'default'})
-  )+'\n'+'   ===============================================')
-  .option('-l, --list', 'list of formal released Solidity Compiler')
+exports.handler = function(callback, event, context) {
+  
+  var web3 = new Web3( new Web3.providers.HttpProvider( net_to_provider[event.chain] ));
 
+  var input = event.source;
+  var solc_version = event.compilerVersion;
+  var is_optimized = event.optimizer;
+  var contract_address = event.adr;
+  var runs = event.runs;
+  var cname = event.name;
 
-program
-  .command('verify <chainChoice>')
-  .description('Verify a contract against bytecode on Blockchain')
-  .action((chainChoice) => {
-    if (typeof chainChoice == 'undefined'){
-      console.log('Please specify a chain (e.g. mainnet)');
+  solc.loadRemoteVersion(solc_version, function(err, solc_specific){
+    if(err){
+      console.log('Solc failed to loaded'+err);
     }
+   // if solc successfully loaded, compile the contract and get the JSON output
+   var output = solc_specific.compile(input, is_optimized);
+   // get bytecode from JSON output
+
+   if (typeof output['contracts'][':'+cname] === 'undefined'){
+     // if there are more than one contract in the contract file, then the JSON representation will have ":"
+     // at the front, but if there is only the main contract, there won't, in which case the vaule assignment
+     // above will be empty or undefined.
+     var bytecode = output['contracts'][cname]['runtimeBytecode'];
+   }
+   else{
+     var bytecode = output['contracts'][':'+cname]['runtimeBytecode'];
+   }
+
+   if (parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[0].slice(1)) >= 4
+    && parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[1].slice(1)) >= 7){
+     // if solc version is at least 0.4.7, then swarm hash is included into the bytecode.
+     // every bytecode starts with a fixed opcode: "PUSH1 0x60 PUSH1 0x40 MSTORE"
+     // which is 6060604052 in bytecode whose length is 10
+
+     var fixed_prefix= bytecode.slice(0,10);
+     // every bytecode from compiler would have constructor bytecode inserted before actual deployed code.
+     // the starting point is a fixed opcode: "CALLDATASIZE ISZERO"
+     // which is 3615 in bytecode
+     var starting_point = bytecode.search('3615');
+     var remaining_part = bytecode.slice(starting_point);
+     // a165627a7a72305820 is a fixed prefix of swarm info that was appended to contract bytecode
+     // the beginning of swarm_info is always the ending point of the actual contract bytecode
+     var ending_point = bytecode.search('a165627a7a72305820');
+     // construct the actual deployed bytecode
+     bytecode_from_compiler = '0x'+fixed_prefix + bytecode.slice(starting_point, ending_point);
+   }
+   else{
+     bytecode_from_compiler = '0x'+bytecode;
+   }
+   // testify with result from blockchain until the compile finishes.
+   var bytecode_from_blockchain = testify_with_blochchain(solc_version, contract_address);
+
+   if (bytecode_from_blockchain == bytecode_from_compiler)
+    callback(null, "ok");
+  else
+    callback("NA", "NOK")
+
+ });
+};
+
+
+
+
+
+function testify_with_blochchain(solc_version, contract_address){
+  // using web3 getCode function to read from blockchain
+  web3.eth.getCode(contract_address)
+  .then(output =>{
+    if (parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[0].slice(1)) >= 4
+     && parseInt(solc_version.match(/v\d+?\.\d+?\.\d+?[+-]/gi)[0].match(/\.\d+/g)[1].slice(1)) >= 7){
+      // code stored at the contract address has no constructor or contract creation bytecode,
+      // only with swarm metadata appending at the back, therefore to get the actual deployed bytecode,
+      // just slice out the trailing swarm metadata.
+      var ending_point = output.search('a165627a7a72305820');
+
+      var swarm_hash_full = output.slice(output.lastIndexOf("a165627a7a72305820"), -4);
+      var swarm_hash = swarm_hash_full.slice(18);
+
+      bytecode_from_blockchain = output.slice(0,ending_point);
+
+    }
+    // if the solc version is less than 0.4.7, then just directly compared the two.
     else{
-      console.log('You\'ve chosen: '+chainChoice);
+      bytecode_from_blockchain = output;
 
-      const net_to_provider = {
-        'main':'https://chain.hyperdapp.org',
-        'ashoka': 'https://ashoka.hyperdapp.org',
-        'shivaji': 'https://shivaji.hyperdapp.org',
-        'chola': 'https://chola.hyperdapp.org',
-      }
-
-      if (chainChoice == 'main' || chainChoice == 'ashoka'
-        || chainChoice=='shivaji' || chainChoice== 'chola'){
-
-        var provider = net_to_provider[chainChoice];
-        // After confirming the chain choice, prompt question.
-        prompt(verifier_question)
-          .then( (answers) =>{
-          answers['file_folder'] = process.cwd();
-          verifier(answers, provider);
-        })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-      else{
-        console.log(chalk.red.bold('Invalid chain choice')+' Your current choice is by default: mainchain');
-        console.log(chalk.green.bold('Please choose from: ')+chalk.underline('main')+' , '+chalk.underline('ashoka')+' , '+
-        chalk.underline('shivaji')+' , '+chalk.underline('chola'));
-      }
     }
+    return bytecode_from_blockchain;
   });
-
-program
-  .command('compiler')
-  .description('Complete compiler version look up (major release plus all nightly commits)')
-  .action(()=>{
-    prompt(compiler_question)
-      .then((answers, provider)=>{
-        compiler_look_up(answers);
-      })
-      .catch(err=>{
-        console.log(err);
-      });
-  });
-
-
-
-  program.parse(process.argv);
-
-  if (program.list) solcVersionList();
+}
---
+++ test.js
@@ -0,0 +1,34 @@
+const solc = require('solc');
+const Web3 = require('web3');
+const fs = require('fs');
+
+var solc_version = 'v0.4.4+commit.4633f3de';
+var file_name = 'MultiSigWallet.sol';
+var v3 = '0x7da82c7ab4771ff031b66538d2fb9b0b047f6cf9';
+var is_optimized = 1;
+var provider = 'https://mainnet.infura.io';
+var file_folder = process.cwd();
+
+var web3 = new Web3( new Web3.providers.HttpProvider(provider));
+
+var file_path = file_folder +'/'+file_name;
+var input = fs.readFileSync(file_path,'utf8');
+solc.loadRemoteVersion(solc_version, function(err, solc_specific){
+  if(err){
+    console.log('Solc failed to loaded'+err);
+  }
+  var output = solc_specific.compile(input, is_optimized);
+
+
+  if (typeof output['contracts'][':'+file_name.slice(0,file_name.length-4)] === 'undefined'){
+    // if there are more than one contract in the contract file, then the JSON representation will have ":"
+    // at the front, but if there is only the main contract, there won't, in which case the vaule assignment
+    // above will be empty or undefined.
+    var bytecode = output['contracts'][file_name.slice(0,file_name.length-4)]['runtimeBytecode'];
+  }
+  else{
+    var bytecode = output['contracts'][':'+file_name.slice(0,file_name.length-4)]['runtimeBytecode'];
+  }
+
+  console.log(">>", bytecode)
+});
